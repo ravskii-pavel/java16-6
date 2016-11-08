c472db8046d701f1613c3e4d3019c18e6186b4ef
ravskii-pavel <ravskii.pavel@gmail.com>
2016-11-08 17:54:04 +0200

    with new file

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
         int num = 9208;
-        String e = (num % 2 == 0) ? "even" : ("odd");
-        System.out.println("Hello World!");
-        System.out.println(num);
-        System.out.println(e);
+         String e = (num % 2 == 0) ? "even" : ("odd");
+         System.out.println("Hello World!");
+         System.out.println(num);
+         System.out.println(e);
     }
 }
