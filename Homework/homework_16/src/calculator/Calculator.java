f437d456894d716158a94d341a9120fb8836d272
Pavel Ravskiy <ravskiy@dn.shambala>
2017-01-18 13:16:14 +0200

    HW_16_done

--- Homework/homework_16/src/calculator/Calculator.java
+++ Homework/homework_16/src/calculator/Calculator.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.Font;
 import javax.swing.border.EtchedBorder;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 /**
  * Created by Ravskiy Pavel on 09.01.2017.
@@ -12,6 +14,7 @@ public class Calculator {
 
     public static JTextField textField = new JTextField("0");
 
+
     public static void main(String[] args) {
 
         JFrame frame = new JFrame("Calculator");
@@ -19,16 +22,13 @@ public class Calculator {
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         textField.setFont(new Font("",Font.PLAIN, 20));
+
         BorderLayout borderLayout = new BorderLayout();
         frame.setLayout(borderLayout);
-        //borderLayout.addLayoutComponent(textField, BorderLayout.NORTH);
-
         frame.add(textField, borderLayout.NORTH);
-
         MainPanel mainPanel = new MainPanel();
-        //borderLayout.addLayoutComponent(mainPanel, BorderLayout.CENTER);
         frame.add(mainPanel, borderLayout.WEST);
         frame.setVisible(true);
-
+        textField.setFocusable(false);
     }
 }
--- Homework/homework_16/src/calculator/MainPanel.java
+++ Homework/homework_16/src/calculator/MainPanel.java
@@ -9,20 +9,10 @@ import java.awt.*;
 public class MainPanel extends JPanel {
 
     public MainPanel() {
-        //GridLayout gridLayout = new GridLayout();
-        //BorderLayout borderLayout = new BorderLayout();
-        //borderLayout.addLayoutComponent(numberPanel, BorderLayout.WEST);
         NumberPanel numberPanel = new NumberPanel();
-
-        //borderLayout.addLayoutComponent(numberPanel, borderLayout.CENTER);
-        //numberPanel.setBorder(BorderFactory.createMatteBorder(1,2,5,1,Color.blue));
         add(numberPanel);
 
         OperationPanel operationPanel =  new OperationPanel();
-
-        //borderLayout.addLayoutComponent(operationPanel, borderLayout.CENTER);
-        //gridLayout.addLayoutComponent("OperationPanel", operationPanel);
         add(operationPanel);
-
     }
 }
